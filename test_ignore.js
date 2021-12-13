f5a42be30c7880d75cd41ac6f0725b72e43e84d0
Pio Su <quantumpio@gmail.com>
2021-12-13 11:52:10 +0100

    Almost finished - deleted some notes - working but  few cleanig works needed- trying to fix github03 and ignore some files

--- test_ignore.js
+++ test_ignore.js
@@ -68,3 +68,70 @@ if (choice === 'sunny' && temperature < 86) {
   }
 
   
+// final messages:
+
+
+   //if (roundCount = 5 && (playerScore > compScore)) {
+   //    alert("You've Won!");
+   //    playerChoice = confirm("Play again?");
+   //    if (playerChoice == true) {
+   //        alert("wybrał win ok");
+   //        //roundCount = 1;
+   //    } else {
+   //        alert("See you nex time!")
+   //    }
+   //    
+   //} 
+   //else if (roundCount = 5 && (playerScore > compScore)) {
+   //    (alert("You've lost!")) 
+   //    playerChoice = confirm("Play again?");
+   //    if (playerChoice == true) {
+   //        alert("wybrał lose ok");
+   //        //roundCount = 1;
+   //    } else {
+   //        alert("See you nex time!")
+   //    }
+
+   //} else { 
+   //    alert("Lets go!");
+   //}
+
+
+
+   if (choice === 'sunny') {
+    if (temperature < 86) {
+      para.textContent = `It is ${temperature} degrees outside — nice and sunny. Let\'s go out to the beach, or the park, and get an ice cream.`;
+    } else if (temperature >= 86) {
+      para.textContent = `It is ${temperature} degrees outside — REALLY HOT! If you want to go outside, make sure to put some sunscreen on.`;
+    }
+  }
+
+
+
+
+
+  // -----------random functions from internet-------------: 
+var item = items[Math.random()*items.length>>0]
+ // or:
+function random_item(items)
+{
+  
+return items[Math.floor(Math.random()*items.length)];
+     
+}
+
+var items = [254, 45, 212, 365, 2543];
+console.log(random_item(items));
+
+// or:
+
+function random_item(items)
+{
+  
+return items[Math.floor(Math.random()*items.length)];
+     
+}
+
+const items = [254, 45, 212, 365, 2543];
+console.log(random_item(items));
+
